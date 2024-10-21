04fe7828f144dfe8b9fca04e135c963e4bb8dc52
Olugbenga Raymond <olugbengaraymond20@gmail.com>
2024-10-21 12:30:22 +0100

    correcting the user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,16 +11,24 @@ class UserController extends Controller
 {
     // Registration of New User
     function register(Request $request){
-        $user = new User;
-        $user->firstname = $request->input('firstname');
-        $user->lastname = $request->input('lastname');
-        $user->email = $request->input('email');
-        $user->phone = $request->input('phone');
-        $user->username = $request->input('username');
-        $user->user_role = $request->input('user_role');
-        $user->password = Hash::make($request->input('password'));
-        $user->user_picture = $request->file('user_picture')->store('user_dp');
-        $user->save();
-        return $user;
+        try {
+            $user = new User;
+            $user->firstname = $request->input('firstname');
+            $user->lastname = $request->input('lastname');
+            $user->email = $request->input('email');
+            $user->phone = $request->input('phone');
+            $user->username = $request->input('username');
+            $user->user_role = $request->input('user_role');
+            $user->password = Hash::make($request->input('password'));
+            if($request->hasFile('user_picture')) {
+                $user->user_picture = $request->file('user_picture')->store('user_dp');
+            } else {
+                $user->user_picture = 'default_dp.jpg'; // Set a default profile picture if no image is provided.
+            }
+            $user->save();
+            return response() -> json($user, 201);
+        } catch(\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
 }
